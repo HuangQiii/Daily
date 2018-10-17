00193f77fb6659cbb1915f11703f31b7486cff19
HuangQiii <598260403@qq.com>
2018-10-17 22:53:55 +0800

    [ADD] add try and is

---
+++ 1017-try/is.js
@@ -0,0 +1,9 @@
+export const isUndefined = val => typeof val === 'undefined'
+
+export const isNull = val => val === null
+
+export const isFunction = val => typeof val === 'function'
+
+export const isString = val => typeof val === 'string'
+
+export const isExist = val => !(isUndefined(val) || isNull(val))
